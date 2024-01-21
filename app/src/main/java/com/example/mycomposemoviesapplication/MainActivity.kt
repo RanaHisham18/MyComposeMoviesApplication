0a5e525e23d6d304e57f8d2f617217fcb6a64fe6
RanaHisham18 <hishamrana00@gmail.com>
2024-01-21 17:18:34 +0200

    Make the app bar and the card with image and text

--- app/src/main/java/com/example/mycomposemoviesapplication/MainActivity.kt
+++ app/src/main/java/com/example/mycomposemoviesapplication/MainActivity.kt
@@ -1,46 +1,143 @@
 package com.example.mycomposemoviesapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.mycomposemoviesapplication.ui.theme.MyComposeMoviesApplicationTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeMoviesApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun MyApp(content: @Composable() () -> Unit) {
+    //container function
+    MyComposeMoviesApplicationTheme {
+        Scaffold(topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("My Movie App")
+                }
+            )
+        }) {
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun MainContent(
+    movieList: List<String> = listOf(
+        "Avatar",
+        "300",
+        "Harry Potter",
+        "Life",
+        "In the shadow of the moon",
+        "Titanic",
+        "Lala Land"
     )
+) {
+    MyApp {}
+    Column(
+        modifier = Modifier
+            .padding(25.dp, 100.dp, 25.dp, 25.dp)
+            .fillMaxWidth()
+    ) {
+        LazyColumn {
+            items(items = movieList) {
+                MovieRow(movie = it)
+            }
+        }
+
+    }
+}
+
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(150.dp),
+        shape = RoundedCornerShape(corner = CornerSize(12.dp))
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp), shape = RectangleShape, shadowElevation = 24.dp
+            ) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie Image")
+            }
+            Text(text = movie ,
+                modifier = Modifier.padding(4.dp),
+                style = TextStyle(
+                    fontSize = 18.sp,
+
+                )
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyComposeMoviesApplicationTheme {
-        Greeting("Android")
+        MyApp {
+            MainContent()
+
+        }
     }
 }
